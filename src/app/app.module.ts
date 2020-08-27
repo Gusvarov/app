6a629b47f92277042cd9a4b83bf9ea1b89dfa240
Oleksandr Terletskyy <mr.terletskyy@gmail.com>
2020-08-27 11:41:40 +0300

    pades fixes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -31,7 +32,8 @@ import { MatDialogModule } from '@angular/material/dialog';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatDialogModule
+    MatDialogModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/count/count.component.ts
+++ src/app/count/count.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./count.component.scss']
 })
 export class CountComponent implements OnInit {
+  counter: number = 2;
+  reward = '';
 
+  circles = [
+    { id: 1 },
+    { id: 2 },
+  ]
   constructor() { }
 
   ngOnInit(): void {
   }
+  increment() {
+    if (this.counter < 20) {
+      ++this.counter;
+      this.circles.push({
+        id: this.counter
+      })
 
+    } else {
+      return null
+    }
+  }
+  decrement() {
+    if (this.counter > 2) {
+      --this.counter;
+      this.circles.pop()
+
+    } else {
+      return null
+    }
+  }
 }
+
