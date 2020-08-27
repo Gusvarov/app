f9bcbebce755a02f41b21cbf31f7943be80584c4
Oleksandr Terletskyy <mr.terletskyy@gmail.com>
2020-08-27 17:37:01 +0300

    pictures new

--- src/app/phone/phone.component.ts
+++ src/app/phone/phone.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, Input, Output } from '@angular/core';
 import { DataService } from '../data.service';
-
+import { map } from 'rxjs/operators'
 @Component({
   selector: 'app-phone',
   templateUrl: './phone.component.html',
   styleUrls: ['./phone.component.scss']
 })
 export class PhoneComponent implements OnInit {
-  cardData
+  cardData$;
+  templatePath = '../../assets/images/1.png'
   reward = ''
   circles = [
     // { id: 1 },
@@ -17,7 +18,14 @@ export class PhoneComponent implements OnInit {
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
-    this.cardData = this.dataService.cardData.value
+    // this.cardData$ = this.dataService.cardData
+    // templatePath=this.dataService.cardData.pipe(
+    //   .map(this.cardData$ =>{
+    //     return card
+    //   })
+    // )
+    console.log(this.cardData$)
+    // this.cardData = this.dataService.cardData.value
     // let a = this.dataService.cardData.value.configuration.templateId
     // console.log(a)
 
